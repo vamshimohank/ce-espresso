e64b5844277b2e89d49ee5a1606c6496b56cda7d
dceresoli@gmail.com <dceresoli@gmail.com@5549f3ee-7223-ea95-221b-f18de8df08b8>
2011-10-18 12:54:25 +0000

    Sync QE to CE.

--- trunk/PHonon/PH/elph.f90
+++ trunk/PHonon/PH/elph.f90
@@ -12,10 +12,13 @@ MODULE el_phon
   !
   SAVE
   !
+  LOGICAL :: elph, elph_mat, elph_simple
   INTEGER :: elph_nbnd_min, elph_nbnd_max
   INTEGER :: el_ph_ngauss, el_ph_nsigma
+  INTEGER, allocatable :: wan_index_dyn(:)
   REAL(DP) :: el_ph_sigma
   COMPLEX(DP), ALLOCATABLE, TARGET :: &
        el_ph_mat(:,:,:,:)    !  nbnd, nbnd, nks, 3*nat
+  CHARACTER (LEN=256) :: auxdvscf
   !
 END MODULE el_phon

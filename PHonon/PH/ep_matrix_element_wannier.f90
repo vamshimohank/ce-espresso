eb3949db89f25162d7fd2eb6d3456ee357f0659b
dceresoli <dceresoli@gmail.com>
2011-10-18 12:54:25 +0000

    Sync QE to CE.

--- PHonon/PH/ep_matrix_element_wannier.f90
+++ PHonon/PH/ep_matrix_element_wannier.f90
@@ -21,13 +21,14 @@ SUBROUTINE ep_matrix_element_wannier()
   USE dynmat, ONLY : dyn, w2
   USE qpoint, ONLY : xq, nksq, ikks
   USE modes,  ONLY : npert, nirr
-  USE control_ph, ONLY : trans, elph_mat, dvscf_dir
+  USE control_ph, ONLY : trans, dvscf_dir
   USE units_ph, ONLY : iudyn, lrdrho, iudvscf
   USE io_global, ONLY : stdout
   USE mp_global, ONLY : me_pool, root_pool
   USE modes, ONLY : u
   USE klist, ONLY : xk
   USE wvfct, ONLY : npwx
+  USE el_phon, ONLY: elph_mat
   !
   IMPLICIT NONE
   !
@@ -148,7 +149,7 @@ SUBROUTINE ep_matrix_element_wannier()
 END SUBROUTINE ep_matrix_element_wannier
 
 !-----------------------------------------------------------------------
-SUBROUTINE elphsum_wannier
+SUBROUTINE elphsum_wannier(q_index)
   !-----------------------------------------------------------------------
   !
   !      Sum over BZ of the electron-phonon matrix elements el_ph_mat
@@ -179,6 +180,11 @@ SUBROUTINE elphsum_wannier
   USE mp,        ONLY: mp_sum
   !
   IMPLICIT NONE
+  !
+  INTEGER :: q_index
+  !
+  !
+
   ! eps = 20 cm^-1, in Ry
   REAL(DP) :: eps
   PARAMETER (eps = 20.d0 / 13.6058d0 / 8065.5d0)
@@ -208,12 +214,13 @@ SUBROUTINE elphsum_wannier
   COMPLEX(DP) :: el_ph_sum (3*nat,3*nat), dyn_corr(3*nat,3*nat)
 
   INTEGER, EXTERNAL :: find_free_unit
+  CHARACTER (LEN=6), EXTERNAL :: int_to_char
 
   nmodes=3*nat
 
   write(filelph,'(A5,f9.6,A1,f9.6,A1,f9.6)') 'elph.',xq(1),'.',xq(2),'.',xq(3)
-!  write(file_elphmat,'(A8)') 'elph.mat'
-  file_elphmat=prefix//'elph.mat'
+  file_elphmat=trim(adjustl(prefix))//'_elph.mat.q_'// TRIM( int_to_char( q_index ) )
+
   ! parallel case: only first node writes
   IF ( me_pool /= root_pool ) THEN
      iuelph = 0
@@ -695,15 +702,15 @@ subroutine get_equivalent_kpq(xk,xq,kpq,g_kpq, igqg)
   end do
 
 
-  write(stdout,'(1x,a)') '+-------------------------------------+' 
-  write(stdout,'(1x,a)') '|   k    k+q        G        igqg     |'
-  write(stdout,'(1x,a)') '| ----  ------  ---------- -----------|'
-  
-  do k=1,nksq
-     write(stdout,'(6i6)') k,kpq(k),(g_kpq(i,k),i=1,3),igqg(k)
-     !       write(stdout,'(6f10.4,3i4)')(kpt_latt(i,k),i=1,3),(kpt_latt(i,indexkpq(k)),i=1,3),(g_kpq(i,k),i=1,3)
-  enddo
-  write(stdout,'(1x,a)') '+-------------------------------------------------+' 
+!  write(stdout,'(1x,a)') '+-------------------------------------+' 
+!  write(stdout,'(1x,a)') '|   k    k+q        G        igqg     |'
+!  write(stdout,'(1x,a)') '| ----  ------  ---------- -----------|'
+!  
+!  do k=1,nksq
+!     write(stdout,'(6i6)') k,kpq(k),(g_kpq(i,k),i=1,3),igqg(k)
+!     !       write(stdout,'(6f10.4,3i4)')(kpt_latt(i,k),i=1,3),(kpt_latt(i,indexkpq(k)),i=1,3),(g_kpq(i,k),i=1,3)
+!  enddo
+!  write(stdout,'(1x,a)') '+-------------------------------------------------+' 
 
   deallocate(xk_crys)
 
@@ -793,64 +800,3 @@ subroutine calculate_and_apply_phase(ik, ikqg, igqg, npwq_refolded, g_kpq, xk_ga
   return
 end subroutine calculate_and_apply_phase
   
-!
-! Copyright (C) 2001 PWSCF group
-! This file is distributed under the terms of the
-! GNU General Public License. See the file `License'
-! in the root directory of the present distribution,
-! or http://www.gnu.org/copyleft/gpl.txt .
-!
-!
-!-----------------------------------------------------------------------
-subroutine trnvect (vect, at, bg, iflag)
-  !-----------------------------------------------------------------------
-  !
-  !  This routine transforms a vector (like forces which in the
-  !  crystal axis is represented on the basis of the reciprocal lattice
-  !  vectors) from crystal to cartesian axis (iflag.gt.0)
-  !  and viceversa (iflag.le.0)
-  !
-  USE kinds
-  implicit none
-  integer :: iflag
-  ! input: gives the versus of the transformati
-
-  real(DP) :: vect (3), at (3, 3), bg (3, 3)
-  ! inp/out: the vector to transform
-  ! input: direct lattice vectors
-  ! input: reciprocal lattice vectors
-  real(DP) :: work (3)
-  ! a working array
-
-  integer :: ipol, ialpha
-  ! counter on crystal coordinates
-  ! counter on cartesian coordinates
-  if (iflag.gt.0) then
-     !
-     !     forward transformation, from crystal to cartesian axis
-     !
-     do ipol = 1, 3
-        work (ipol) = vect (ipol)
-     enddo
-     do ialpha = 1, 3
-        vect (ialpha) = 0.d0
-        do ipol = 1, 3
-           vect (ialpha) = vect (ialpha) + work (ipol) * bg (ialpha, ipol)
-        enddo
-     enddo
-  else
-     !
-     !    backward transformation, from cartesian to crystal axis
-     !
-     do ipol = 1, 3
-        work (ipol) = 0.d0
-        do ialpha = 1, 3
-           work (ipol) = work (ipol) + vect (ialpha) * at (ialpha, ipol)
-        enddo
-     enddo
-     do ipol = 1, 3
-        vect (ipol) = work (ipol)
-     enddo
-  endif
-  return
-end subroutine trnvect

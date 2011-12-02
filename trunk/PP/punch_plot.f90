dfffba42acbf462500374352d38e940243a129f4
dceresoli@gmail.com <dceresoli@gmail.com@5549f3ee-7223-ea95-221b-f18de8df08b8>
2011-12-02 13:56:52 +0000

    Reduced density gradient as plot_num=19

--- trunk/PP/punch_plot.f90
+++ trunk/PP/punch_plot.f90
@@ -246,6 +246,12 @@ SUBROUTINE punch_plot (filplot, plot_num, sample_bias, z, dz, &
      ELSE
         CALL errore('punch_plot','B_xc available only when noncolin=.true.',1)
      ENDIF
+
+  ELSEIF (plot_num == 19) THEN    ! reduced density gradient
+
+     IF (noncolin) CALL errore('punch_plot','rdg+noncolin not yet implemented',1)
+     CALL do_rdg (raux)           ! in elf.f90
+
   ELSE
 
      CALL infomsg ('punch_plot', 'plot_num not implemented')
